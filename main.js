9c93d3998b5b00fced14d4c5795a05ca95514079
Jan Greger Hemb <jan.greger@gmail.com>
2016-09-19 21:06:42 +0200

    Added playing against random AI

--- main.js
+++ main.js
@@ -14,11 +14,17 @@ $( document ).ready(function() {
 	// do not pick up pieces if the game is over
 	// only pick up pieces for the side to move
 	var onDragStart = function(source, piece, position, orientation) {
-		if (game.game_over() === true ||
-			(game.turn() === 'w' && piece.search(/^b/) !== -1) ||
-			(game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+		// if (game.game_over() === true ||
+		// 	(game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+		// 	(game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+		// 		return false;
+		// }
+
+		//only white may move, on their turn
+		if (game.in_checkmate() === true || game.in_draw() === true || piece.search(/^b/) !== -1) {
 				return false;
 		}
+
 	};
 
 	var onDrop = function(source, target) {
@@ -33,12 +39,26 @@ $( document ).ready(function() {
 		if (move === null) return 'snapback';
 
 		updateStatus();
+
+		window.setTimeout(makeRandomMove, 250);
 	};
 
 	// update the board position after the piece snap
 	// for castling, en passant, pawn promotion
 	var onSnapEnd = function() {
 		board.position(game.fen());
+	};
+
+	var makeRandomMove = function() {
+		var possibleMoves = game.moves();
+
+		// game over
+		if (possibleMoves.length === 0) return;
+
+		var randomIndex = Math.floor(Math.random() * possibleMoves.length);
+		game.move(possibleMoves[randomIndex]);
+		board.position(game.fen());
+		updateStatus();
 	};
 
 	var updateStatus = function() {
@@ -75,6 +95,9 @@ $( document ).ready(function() {
 		statusEl.html(status);
 		fenEl.html(game.fen());
 		pgnEl.html(game.pgn());
+
+
+
 	};
 
 	var cfg = {
