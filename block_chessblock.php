75b9b88f2e451d09e0c3aff82aaaa8e1e1109a0c
Jan Greger Hemb <jan.greger@gmail.com>
2016-09-19 21:32:05 +0200

    Converted to tabs <3

--- block_chessblock.php
+++ block_chessblock.php
@@ -4,37 +4,37 @@ require_once($CFG->libdir . '/pagelib.php');
 
 class block_chessblock extends block_base {
 
-	private $jsLoaded = false;
+		private $jsLoaded = false;
 
-    public function init() {
+		public function init() {
 		GLOBAL $PAGE;
 
-        $this->title = get_string('chessblock', 'block_chessblock');
+		$this->title = get_string('chessblock', 'block_chessblock');
 
 
 
 
-    }
+	}
 
 	public function applicable_formats() {
-        return array('all' => true);
-    }
+		return array('all' => true);
+	}
 
 	public function get_content() {
 
-        global $CFG, $OUTPUT, $USER, $DB, $PAGE;
+		global $CFG, $OUTPUT, $USER, $DB, $PAGE;
 
 
 
-        if ($this->content !== null) {
-          return $this->content;
-        }
+		if ($this->content !== null) {
+			return $this->content;
+		}
 
-        $this->content         =  new stdClass;
+		$this->content         =  new stdClass;
 		$this->content->text = "";
 
-        //first element with no .=, just =
-        $this->content->text .= "<h2>Chess!</h2>";
+		//first element with no .=, just =
+		$this->content->text .= "<h2>Chess!</h2>";
 		$this->content->text .= '<button id="newChessGame">New Game of AI vs me chess</button><br>';
 		$this->content->text .= '<div id="board" style="width: 300px"></div>';
 		$this->content->text .= '<p>Status: <span id="status"></span></p>';
@@ -51,9 +51,9 @@ class block_chessblock extends block_base {
 			$PAGE->requires->js('/blocks/chessblock/main.js?'.rand());
 		}
 
-        $this->content->footer = 'The end of my chess block';
+		$this->content->footer = 'The end of my chess block';
 
-        return $this->content;
+		return $this->content;
 
-    }
+	}
 }
