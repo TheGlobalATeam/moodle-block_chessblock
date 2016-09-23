23405ac7b6f786f8af298ff1668931546defac28
Nikita Uvarov <uv.nikita@gmail.com>
2016-09-23 14:04:57 +0200

    Add blockstring string to chessblock strings

--- lang/en/block_chessblock.php
+++ lang/en/block_chessblock.php
@@ -1,5 +1,6 @@
 <?php
 $string['pluginname'] = 'Chess Block';
 $string['chessblock'] = 'Chess Block';
+$string['blockstring'] = 'Chess Block';
 $string['chessblock:addinstance'] = 'Add a new simple chess block';
 $string['chessblock:myaddinstance'] = 'Add a new simple chess block to the My Moodle page';
