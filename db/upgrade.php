6ad026917c240bf5d004177cd54aeed76101c405
Thomas Mellemseter <Thomas Mellemseter>
2016-09-23 13:51:52 +0200

    Update chesstable structure

--- db/upgrade.php
+++ db/upgrade.php
@@ -1,40 +1,50 @@
 <?php
 
 function xmldb_block_chessblock_upgrade($oldversion) {
-	global $CFG, $DB;
+    global $CFG, $DB;
 
-	$result = TRUE;
+    $result = TRUE;
 
-	//NOT IN THE DOCUMENTATION!
-	$dbman = $DB->get_manager(); // loads ddl manager and xmldb classes
+    // loads ddl manager and xmldb classes
+	$dbman = $DB->get_manager();
 
-	//not working
-	//if ($oldversion < 2016021202) {
+	if ($oldversion < 2016092304) {
 
-        // Define table block_chessblock_positions to be created.
+        // Drop unused tables
         $table = new xmldb_table('block_chessblock_positions');
+        if ($dbman->table_exists($table)) {
+            $dbman->drop_table($table);
+        }
 
-        // Adding fields to table block_chessblock_positions.
+        $table = new xmldb_table('block_chessblock_states');
+        if ($dbman->table_exists($table)) {
+            $dbman->drop_table($table);
+        }
+
+        $table = new xmldb_table('block_chessblock_games');
+        if ($dbman->table_exists($table)) {
+            $dbman->drop_table($table);
+        }
+
+        // Create new table
+        $table = new xmldb_table('block_chessblock_games');
+
+        // Adding fields to table block_chessblock_games.
         $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
         $table->add_field('game_fen', XMLDB_TYPE_TEXT, null, null, null, null, null);
         $table->add_field('game_pgn', XMLDB_TYPE_TEXT, null, null, null, null, null);
-        $table->add_field('user_id', XMLDB_TYPE_INTEGER, '20', null, null, null, null);
-        $table->add_field('player_color', XMLDB_TYPE_INTEGER, '1', null, null, null, null);
+        $table->add_field('black_user_id', XMLDB_TYPE_INTEGER, '20', null, null, null, null);
+        $table->add_field('white_user_id', XMLDB_TYPE_INTEGER, '20', null, null, null, null);
 
-        // Adding keys to table block_chessblock_positions.
+        // Adding keys to table block_chessblock_games.
         $table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
 
-        // Conditionally launch create table for block_chessblock_positions.
+        // Conditionally launch create table for block_chessblock_games.
         if (!$dbman->table_exists($table)) {
             $dbman->create_table($table);
         }
 
-        // Chessblock savepoint reached.
-		//wat verson number ???
-        upgrade_block_savepoint(true, 2016021205, 'chessblock');
-    //}
-
-	// Insert PHP code from XMLDB Editor here
-
+        upgrade_block_savepoint(true, 2016092304, 'chessblock');
+    }
 	return $result;
 }
