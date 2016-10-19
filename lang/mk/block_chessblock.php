77866e099370483d35989be47619aa49a2370936
Thomas Mellemseter <Thomas Mellemseter>
2016-10-19 14:43:43 +0200

    Add mackedonian language

---
+++ lang/mk/block_chessblock.php
@@ -0,0 +1,21 @@
+<?php
+$string['pluginname'] = 'шах блок';
+$string['chessblock'] = 'шах блок';
+$string['blockstring'] = 'шах блок';
+$string['chessblock:addinstance'] = 'Add a new simple chess block';
+$string['chessblock:myaddinstance'] = 'Add a new simple chess block to the My Moodle page';
+
+// Static html strings
+$string['blocktitle'] = 'шах';
+$string['newgamebutton'] = 'Започнете нова игра против компјутерот';
+$string['gamestatus'] = 'статус';
+$string['userid'] = 'ID на корисникот';
+
+// Dynamic js strings
+$string['white'] = 'Белo';
+$string['black'] = 'црнo';
+$string['tomove'] = 'пpemectи';
+$string['gameover'] = 'играта заврши';
+$string['isincheck'] = 'is in check';
+$string['isincheckmate'] = 'mat';
+$string['drawnposition'] = 'нерешено';
