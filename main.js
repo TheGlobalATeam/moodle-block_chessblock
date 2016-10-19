9bf5f297d4d45ca83dc98121933f26dd3e6afe6f
Thomas Mellemseter <Thomas Mellemseter>
2016-10-19 15:35:52 +0200

    Fixed indentation + added highlight legal moves

--- main.js
+++ main.js
@@ -7,60 +7,94 @@ function loadLanguage(Y, currentLanguage) {
 
 $( document ).ready(function() {
 
-	var board;
-	var game = new Chess();
-	var statusEl = $('#status');
-	var gameRunning = false;
+    var board;
+    var game = new Chess();
+    var statusEl = $('#status');
+    var gameRunning = false;
 
-	// do not pick up pieces if the game is over
-	// only pick up pieces for the side to move
-	var onDragStart = function(source, piece, position, orientation) {
-		// if (game.game_over() === true ||
-		// 	(game.turn() === 'w' && piece.search(/^b/) !== -1) ||
-		// 	(game.turn() === 'b' && piece.search(/^w/) !== -1)) {
-		// 		return false;
-		// }
+    // do not pick up pieces if the game is over
+    // only pick up pieces for the side to move
+    var onDragStart = function(source, piece, position, orientation) {
+        // if (game.game_over() === true ||
+        // 	(game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+        // 	(game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+        // 		return false;
+        // }
 
-		//only white may move, on their turn
-		if (game.in_checkmate() === true ||
-			game.in_draw() === true ||
-			piece.search(/^b/) !== -1) {
-			return false;
-		}
-	};
+        //only white may move, on their turn
+        if (game.in_checkmate() === true ||
+            game.in_draw() === true ||
+            piece.search(/^b/) !== -1) {
+                return false;
+        }
+    };
 
-	var onDrop = function(source, target) {
-		// see if the move is legal
-		var move = game.move({
-			from: source,
-			to: target,
-			promotion: 'q' // NOTE: always promote to a queen for example simplicity
-		});
+    var onDrop = function(source, target) {
+        removeHighlight();
 
-		// illegal move
-		if (move === null) return 'snapback';
+        // see if the move is legal
+        var move = game.move({
+            from: source,
+            to: target,
+            promotion: 'q' // NOTE: always promote to a queen for example simplicity
+        });
 
-		updateStatus();
-		window.setTimeout(makeRandomMove, 250);
-	};
+        // illegal move
+        if (move === null) return 'snapback';
 
-	// update the board position after the piece snap
-	// for castling, en passant, pawn promotion
-	var onSnapEnd = function() {
-		board.position(game.fen());
-	};
+        updateStatus();
+        window.setTimeout(makeRandomMove, 250);
+    };
 
-	var makeRandomMove = function() {
-		var possibleMoves = game.moves();
+    // update the board position after the piece snap
+    // for castling, en passant, pawn promotion
+    var onSnapEnd = function() {
+        board.position(game.fen());
+    };
 
-		// game over
-		if (possibleMoves.length === 0) return;
+    var removeHighlight = function(square) {
+        $('#board .square-55d63').css('background', '');
+    }
 
-		var randomIndex = Math.floor(Math.random() * possibleMoves.length);
-		game.move(possibleMoves[randomIndex]);
-		board.position(game.fen());
-		updateStatus();
-	};
+    var highlightSquare = function(square) {
+        var squareElement = $('#board .square-' + square);
+        var isSqaureBlack = (squareElement.hasClass('black-3c85d') === true);
+        var backgroundColor = (isSqaureBlack)? '#696969': '#a9a9a9';
+        squareElement.css('background', backgroundColor);
+    }
+
+    var onMouseoverSquare = function(square, piece) {
+        console.log('Over ' + square);
+        // Get possible moves for this square:
+        var moves = game.moves({
+            square: square,
+            verbose: true
+        })
+
+        if (moves.length === 0) return;
+
+        // Highlight hovered square and leagl moves:
+        highlightSquare(square);
+        for (var i = 0; i < moves.length; ++i) {
+            highlightSquare(moves[i].to);
+        }
+    }
+
+    var onMouseoutSquare = function(square, piece) {
+        removeHighlight();
+    }
+
+    var makeRandomMove = function() {
+        var possibleMoves = game.moves();
+
+        // game over
+        if (possibleMoves.length === 0) return;
+
+        var randomIndex = Math.floor(Math.random() * possibleMoves.length);
+        game.move(possibleMoves[randomIndex]);
+        board.position(game.fen());
+        updateStatus();
+    };
 
     var updateStatus = function() {
         var status = '';
@@ -76,90 +110,74 @@ $( document ).ready(function() {
                 status += ', ' + moveColor + ' ' + language['isincheck'];
             }
         }
-		statusEl.html(status);
-	};
+        statusEl.html(status);
+    };
 
-	var saveGame = function(){
+    var saveGame = function(){
 
-		if(!gameRunning){
-			console.log("need to start a game in order to save");
-			return;
-		}
+        if(!gameRunning){
+            console.log("need to start a game in order to save");
+            return;
+        }
 
-		//CAN't FIND any documentation...
-		//POST to api
-		Y.io(
-			M.cfg.wwwroot + "/blocks/chessblock/api/post_game_data.php", {
-				method: "POST",
-				data: 'gameFEN='+game.fen()+'&gamePGN=NOPE',
-				on: {
-					success: function(io, o, arguments) {
+        //CAN't FIND any documentation...
+        //POST to api
+        Y.io(
+            M.cfg.wwwroot + "/blocks/chessblock/api/post_game_data.php", {
+                method: "POST",
+                data: 'gameFEN='+game.fen()+'&gamePGN=NOPE',
+                on: {
+                    success: function(io, o, arguments) {
+                        console.log("SAVED!");
+                        //console.log(o.response);
+                    }
+                }
+            }
+        );
+    }
 
-						console.log("SAVED!");
-						//console.log(o.response);
+    var cfg = {
+        draggable: true,
+        position: 'start',
+        onDragStart: onDragStart,
+        onMouseoutSquare: onMouseoutSquare,
+        onMouseoverSquare: onMouseoverSquare,
+        onDrop: onDrop,
+        onSnapEnd: onSnapEnd,
+        pieceTheme: M.cfg.wwwroot + '/blocks/chessblock/chessboardjs/img/chesspieces/wikipedia/{piece}.png',
+    };
 
-					}
-				}
-			}
-		);
+    updateStatus();
 
-	}
-
-	var cfg = {
-	  draggable: true,
-	  position: 'start',
-	  onDragStart: onDragStart,
-	  onDrop: onDrop,
-	  onSnapEnd: onSnapEnd,
-	  pieceTheme: M.cfg.wwwroot + '/blocks/chessblock/chessboardjs/img/chesspieces/wikipedia/{piece}.png',
-	};
-
-	updateStatus();
-
-
-
-	$('#saveChessGame').click(function(){
-
-		saveGame();
-
-	});
+    $('#saveChessGame').click(saveGame());
 
 	$('#newChessGame').click(function(){
+        game = new Chess();
+        //start fen
+        cfg.position = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+        board = new ChessBoard('board', cfg);
+        gameRunning = true;
+        updateStatus();
+    });
 
-		game = new Chess();
-		//start fen
-		cfg.position = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-		board = new ChessBoard('board', cfg);
-		gameRunning = true;
-
-		updateStatus();
-	});
-
-	$('#loadPrevChessGame').click(function(){
-
-		//Loading from API
-		Y.io(
-			M.cfg.wwwroot + "/blocks/chessblock/api/get_game_data.php", {
-				method: "GET",
-				on: {
-					success: function(io, o, arguments) {
-
-						var gameData = JSON.parse(o.response);
-						if( gameData.gameData.game_fen != null &&  gameData.gameData.game_fen.length > 0){
-							var cfgLoad = cfg;
-							cfgLoad.position = gameData.gameData.game_fen;
-
-							game = new Chess(gameData.gameData.game_fen);
-							board = new ChessBoard('board', cfgLoad);
-							gameRunning = true;
-
-						}
-
-					}
-				}
-			}
-		);
-
-	});
-
+    $('#loadPrevChessGame').click(function(){
+        //Loading from API
+        Y.io(
+            M.cfg.wwwroot + "/blocks/chessblock/api/get_game_data.php", {
+                method: "GET",
+                on: {
+                    success: function(io, o, arguments) {
+                        var gameData = JSON.parse(o.response);
+                        if( gameData.gameData.game_fen != null &&  gameData.gameData.game_fen.length > 0){
+                            var cfgLoad = cfg;
+                            cfgLoad.position = gameData.gameData.game_fen;
+                            game = new Chess(gameData.gameData.game_fen);
+                            board = new ChessBoard('board', cfgLoad);
+                            gameRunning = true;
+                        }
+                    }
+                }
+            }
+        );
+    });
 });
