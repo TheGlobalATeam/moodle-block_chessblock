fa18a5132cca0b245718cdda4032cf88e2e60192
Ardit Dika <dika.ardit@gmail.com>
2016-10-21 14:32:53 +0200

    Code refactoring in upgrade.php.

--- db/upgrade.php
+++ db/upgrade.php
@@ -1,16 +1,31 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+defined('MOODLE_INTERNAL') || die();
 function xmldb_block_chessblock_upgrade($oldversion) {
     global $CFG, $DB;
 
-    $result = TRUE;
+    $result = true;
 
-    // loads ddl manager and xmldb classes
-	$dbman = $DB->get_manager();
+    // Loads ddl manager and xmldb classes.
+    $dbman = $DB->get_manager();
 
-	if ($oldversion < 2016092305) {
+    if ($oldversion < 2016092305) {
 
-        // Drop unused tables
+        // Drop unused tables.
         $table = new xmldb_table('block_chessblock_positions');
         if ($dbman->table_exists($table)) {
             $dbman->drop_table($table);
@@ -26,7 +41,7 @@ function xmldb_block_chessblock_upgrade($oldversion) {
             $dbman->drop_table($table);
         }
 
-        // Create new table
+        // Create new table.
         $table = new xmldb_table('block_chessblock_games');
 
         // Adding fields to table block_chessblock_games.
@@ -46,5 +61,5 @@ function xmldb_block_chessblock_upgrade($oldversion) {
 
         upgrade_block_savepoint(true, 2016092305, 'chessblock');
     }
-	return $result;
+    return $result;
 }
